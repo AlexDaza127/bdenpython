1afd2319157819afece18c8bfb1264131299526e
Michael <Michael@DESKTOP-3NNSN6J>
2019-12-09 19:23:00 -0500

    se crean las variables para la conexion, consulta, ejecucion; para guardar registros y luego mostrarlos por medio de un ciclo for

--- bd.py
+++ bd.py
@@ -1,17 +1,26 @@
 #importamos el conector de mysql
 import mysql.connector
 
-#creacion de la conexion con la base de datos en mysql
-dbConnector = {
+#creación de la conexion con la base de datos en mysql
+dbConnect = {
     'host': 'localhost',
     'user': 'root',
     'password':'',
     'database':'pruebapython'
 }
 
-
+#creación de la variable que establece la conexion
 conexion = mysql.connector.connect(**dbConnect)
+#creación del cursor que permite enviar comandos al servidor y recibir los registros
 cursor = conexion.cursor()
+#creacion de la variable que permite realizar la consulta
 sql = "select * from usuarios"
+#se ejecuta la consulta
 cursor.execute(sql)
+#creacion de la variable que recibe los registros de la consulta
 resultado = cursor.fetchall()
+
+
+#se crea un for para imprimir por linea cada fila de la consulta
+for datos in resultado:
+    print(datos)
