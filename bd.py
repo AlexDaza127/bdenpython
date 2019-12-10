13b3f0765d9378a5bb8af07f1bbd4a5ca1ff45d3
Michael <Michael@DESKTOP-3NNSN6J>
2019-12-09 19:17:17 -0500

    #creación de la variable que establece la conexion

--- bd.py
+++ bd.py
@@ -1,16 +1,17 @@
+#importamos el conector de mysql
 import mysql.connector
-dbConnect = {
+
+#creacion de la conexion con la base de datos en mysql
+dbConnector = {
     'host': 'localhost',
     'user': 'root',
     'password':'',
     'database':'pruebapython'
 }
 
+
 conexion = mysql.connector.connect(**dbConnect)
 cursor = conexion.cursor()
 sql = "select * from usuarios"
 cursor.execute(sql)
 resultado = cursor.fetchall()
-
-for datos in resultado:
-    print(datos)
